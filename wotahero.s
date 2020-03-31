93239be05cb3d59407685795d7c9df32d3cbbc3d
Pararaum <pararaum@gmail.com>
2020-03-31 22:07:45 +0200

    First image copied.

--- wotahero.s
+++ wotahero.s
@@ -4,11 +4,15 @@
 	.include	"vicmacros.i"
 	.include	"pseudo16.inc"
 
-
 	.segment "LOADADDR"
 	.export __LOADADDR__
 __LOADADDR__:	.word $0400
 
+	.zeropage
+srcptr:	.word	0
+dstptr:	.word	0
+srcend:	.word	0
+tmpptr:	.word	0
 
 	.segment "STARTUP"
 	cld
@@ -24,19 +28,49 @@ __LOADADDR__:	.word $0400
 	
 
 	.rodata
-	.byte	"Read Only Data"
+	.byte	"Read Only Data (SID)"
 
 
 	.segment "IMAGE"
 	.asciiz	"image"
 
 
+	.data
+image_iwatani:
+	.incbin	"toru_iwatani.bw.c64"
+
 	.code
 _main:
-	lda	#$00
+	lda	#$0b
 	sta	$d020
 	jsr	init
-	P_loadi $2,$e000
+	jsr	cpyimage
+	rts
+
+cpyimage:
+	P_loadi srcend,image_iwatani+8000
+	P_loadi srcptr,image_iwatani
+	P_loadi dstptr,$e000
+	ldy	#0
+l1:	lda	(srcptr),y
+	sta	(dstptr),y
+;;; 	P_storeb srcptr,dstptr
+	P_transfer srcptr,tmpptr
+	P_inc	srcptr
+	P_inc	dstptr
+	P_sub	srcend,tmpptr
+	P_branchNZ tmpptr,l1
+	P_loadi srcend,image_iwatani+8000+1000
+	P_loadi srcptr,image_iwatani+8000
+	P_loadi dstptr,$dc00
+l2:	lda	(srcptr),y
+	sta	(dstptr),y
+	;; 	P_storeb srcptr,dstptr
+	P_transfer srcptr,tmpptr
+	P_inc	srcptr
+	P_inc	dstptr
+	P_sub	srcend,tmpptr
+	P_branchNZ tmpptr,l2
 	rts
 
 init:
