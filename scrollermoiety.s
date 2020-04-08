d4d1796d598ade7ff67974d62cb58c88117eff76
Pararaum <pararaum@gmail.com>
2020-04-08 21:30:37 +0200

    Function scroller_init.

--- scrollermoiety.s
+++ scrollermoiety.s
@@ -1,4 +1,6 @@
 
+	.export	scroller_init
+
 	.include	"pseudo16.inc"
 
 	.importzp	counter16
@@ -6,6 +8,7 @@
 	.bss
 spriteregshadow:
 	.res	2*8		; Sprite positions
+spriteregshadowmsb:
 	.res	1		; Sprite MSBs
 	
 
@@ -16,4 +19,20 @@ scrsprposx:
 	.code
 scroller_init:
 	P_loadi	counter16,0
+	P_loadi	$fe,48
+	ldx	#0
+@l1:	lda	counter16
+	sta	spriteregshadow,x
+	lda	counter16+1
+	beq	@skip
+	lda	spriteregshadowmsb
+	ora	@msbtab,x
+	sta	spriteregshadowmsb
+@skip:
+	P_add	$fe,counter16
+	inx
+	cpx	#8
+	bne	@l1
 	rts
+@msbtab:
+	.byte	1,2,4,8,16,32,64,128
